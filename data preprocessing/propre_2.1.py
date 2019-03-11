e7a4d6372fcbf30010486d4e3872640b775c4437
shen-jiang <44779015+shen-jiang@users.noreply.github.com>
2019-03-11 19:17:13 +0000

    Update propre_2.1.py

--- data preprocessing/propre_2.1.py
+++ data preprocessing/propre_2.1.py
@@ -10,15 +10,8 @@ train = pd.read_csv('/.../train_3.csv')
 valid = pd.read_csv('/.../valid_3.csv')
 test = pd.read_csv('/.../test_3.csv')
 
-
-#
-#   random downsampling
-#
-
 # set a random seed
 rand_seed = 100
-random.seed(rand_seed)
-np.random.seed(rand_seed)
 
 # features for pCTR predictor
 train_out_p = train.loc[train["click"] == 1]
@@ -26,15 +19,11 @@ train_in_p = train.loc[train["click"] == 0].sample(n = 20000, random_state = ran
 
 train_p = pd.concat([train_in_p, train_out_p], axis = 0)
 
-X_p = train_p.loc[:, ((train_p.columns != "click") &
-                    (train_p.columns != "payprice") &
-                    (train_p.columns != "bidprice"))].values
+X_p = train_p.drop(columns = ['bidprice','payprice','click'])
 
 y_p = train_p["click"].values
 
-X_validation = valid.loc[:, ((valid.columns != "click") &
-                                    (valid.columns != "payprice") &
-                                    (valid.columns != "bidprice"))]
+X_validation = valid.drop(columns = ['bidprice','payprice','click'])
 y_validation = valid["click"]
 
 
@@ -51,14 +40,10 @@ train_out_base = train.loc[train["click"] == 1]
 train_in_base = train.loc[train["click"] == 0].sample(n = 40000, random_state = rand_seed).reset_index(drop = True)
 train_base = pd.concat([train_in_base, train_out_base], axis = 0)
 
-X_base = train_base.loc[:, ((train_base.columns != "click") &
-                            (train_base.columns != "payprice") &
-                            (train_base.columns != "bidprice"))].values
+X_base = train_base.drop(columns = ['bidprice','payprice','click']).values
 y_base = train_base["payprice"].values
 
-X_validation_base = valid.loc[:, ((valid.columns != "click") &
-                                        (valid.columns != "payprice") &
-                                        (valid.columns != "bidprice"))]
+X_validation_base = valid.drop(columns = ['bidprice','payprice','click'])
 y_validation_base = valid["payprice"]
 
 X_resampled_base, y_resample_base = X_base, y_base
@@ -66,18 +51,6 @@ X_resampled_base, y_resample_base = X_base, y_base
 #
 #   save
 #
-from sklearn.externals import joblib
-
-joblib.dump(X_resampled_p, "pCTR_X_train_resampled.pkl")
-joblib.dump(y_resampled_p, "pCTR_y_train_resampled.pkl")
-joblib.dump(X_validation_p, "pCTR_X_validation.pkl")
-joblib.dump(y_validation_p, "pCTR_y_validation.pkl")
-
-joblib.dump(X_resampled_base, "basebid_X_train_resampled.pkl")
-joblib.dump(y_resampled_base, "basebid_y_train_resampled.pkl")
-joblib.dump(X_validation_base, "basebid_X_validation.pkl")
-joblib.dump(y_validation_base, "basebid_y_validation.pkl")
-
 
 
 # total_feature
